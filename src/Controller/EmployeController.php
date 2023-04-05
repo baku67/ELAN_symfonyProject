f85f21dcf57521bed616d60e6b25967225c6f5db
= <basile08@hotmail.fr>
2023-04-05 19:11:33 +0200

    WIP recup listEmployes/Entreprise (test sans regarder tuto)

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Employe;
+use App\Entity\Entreprise;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +17,42 @@ class EmployeController extends AbstractController
     {
         return $this->render('employe/index.html.twig', [
             'testKey' => 'testData',
+        ]);
+    }
+
+
+    #[Route('/employesListe', name: 'app_employeList')]
+    public function employeList(EntityManagerInterface $entityManager): Response
+    {
+        $repoEmploye = $entityManager->getRepository(Employe::class);
+        $repoEntreprise = $entityManager->getRepository(Entreprise::class);
+
+        // récupération du tableau d'employés de l'entreprise
+        $employesList = $repoEmploye->findBy();
+
+        return $this->render('employe/employesList.html.twig', [
+            'testKey' => 'testTableauEmployé',
+            'employesArray' => $employesList
+        ]);
+    }
+
+
+    // récupération du tableau d'employés de l'entreprise
+    #[Route('/entrepriseEmployesListe/{idEntreprise}', name: 'app_entrepriseEmployesListe')]
+    public function entrepriseEmployesListe(EntityManagerInterface $entityManager, int $idEntreprise): Response
+    {
+        $repoEmploye = $entityManager->getRepository(Employe::class);
+        $repoEntreprise = $entityManager->getRepository(Entreprise::class);
+
+        $entreprise = $repoEntreprise->find($idEntreprise);
+        $employesList = $repoEmploye->findBy(['entreprise' => $entreprise->getId()]);
+
+        return $this->render('employe/employesList.html.twig', [
+            'testKey' => 'testTableauEmployé',
+            'employesArray' => $employesList,
+            'entreprise' => $entreprise
+            // 'entrepriseName' => $repoEntreprise->find($entreprise)->getRaisonSociale(),
+            // OU 'entreprise' => $repoEntreprise->find($idEntreprise) (et récup le .nom dans la vue),
         ]);
     }
 }
